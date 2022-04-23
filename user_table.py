97c7c214f4beb612cb497426e0b522f01dbf1e71
Nishan <mdnishan006@gmail.com>
2022-04-23 21:09:50 +0530

    "user_table password dtype changed"

--- user_table.py
+++ user_table.py
@@ -13,14 +13,14 @@ cursor = mydb.cursor()
 
 
 # Creating a database named as bank.
-bank_db = 'CREATE DATABASE bank1'
-cursor.execute(bank_db)
+#bank_db = 'CREATE DATABASE bank1'
+#cursor.execute(bank_db)
 
 # Selecting the created database for further use.
 cursor.execute("USE bank1")
 
 # Creating a table named user_table to store the username and password data.
-user_table = "CREATE TABLE user_table(username varchar(50) primary key,password int(10) not null)"
+user_table = "CREATE TABLE user_table(username varchar(50) primary key,password varchar(25) not null)"
 cursor.execute(user_table)
 
 
